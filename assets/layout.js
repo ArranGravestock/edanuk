60319a1690b8ae830b8c3de9eb6332cddb6b15b7
ArranGravestock <arran@gravestock.net>
2020-06-24 22:15:27 +0100

    add contact form stuff

--- assets/layout.js
+++ assets/layout.js
@@ -1104,3 +1104,56 @@ $(window).bind('beforeunload', function(e){
         return 'You are leaving to visit the Edan China, are you sure you want to leave?';
     }
   });
+
+
+
+
+  window.addEventListener("DOMContentLoaded", function() {
+
+    // get the form elements defined in your form HTML above
+    
+    var form = document.getElementById("feedback");
+    var button = document.getElementById("submit");
+    var select = document.getElementById("select");
+    var status = document.getElementById("status");
+
+    // Success and Error functions for after the form is submitted
+    
+    function success() {
+      form.reset();
+      status.innerHTML = "Thanks for your submission!";
+    }
+
+    function error() {
+      status.innerHTML = "Oops! There was a problem.";
+    }
+
+    // handle the form submission event
+
+    form.addEventListener("submit", function(ev) {
+      ev.preventDefault();
+      var data = new FormData(form);
+
+      if(select.value == 'sales') {
+        ajax("POST", "https://formspree.io/myyndypk", data, success, error);
+      } else {
+        ajax("POST", "", data, success, error);
+      }
+    });
+  });
+
+
+  function ajax(method, url, data, success, error) {
+    var xhr = new XMLHttpRequest();
+    xhr.open(method, url);
+    xhr.setRequestHeader("Accept", "application/json");
+    xhr.onreadystatechange = function() {
+      if (xhr.readyState !== XMLHttpRequest.DONE) return;
+      if (xhr.status === 200) {
+        success(xhr.response, xhr.responseType);
+      } else {
+        error(xhr.status, xhr.response, xhr.responseType);
+      }
+    };
+    xhr.send(data);
+  }
